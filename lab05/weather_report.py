17a7b6b2b95f59d9afe98b2ce2a3d4099eb4cdc0
Jonathan <jonathan.olsson8@gmail.com>
2024-07-19 02:35:28 +0200

    arbetade med labb 5

--- lab05/weather_report.py
+++ lab05/weather_report.py
@@ -7,3 +7,16 @@ file_path = Path(__file__).parent / 'temperatur_data.csv'
 
 #read month 2 temperatures from data.csv
 temp_list = file_reader.read_from_file(file_path, 2)
+avg_temp = sum(temp_list)/len(temp_list)
+print(avg_temp)
+
+def calculate_avg_temp(list):
+    sum = 0
+    length = 0
+    for x in list:
+        sum += x
+        length += 1
+    return sum/length
+
+avg_temp = calculate_avg_temp(temp_list)
+print(avg_temp)
