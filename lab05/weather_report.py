d9c9d4e6e78ab2b9bfc9ad3c10b4e045f3124c9d
Jonathan Olsson <jonathan.olsson8@gmail.com>
2024-07-22 13:33:01 +0200

    färdig med labb 5

--- lab05/weather_report.py
+++ lab05/weather_report.py
@@ -8,19 +8,25 @@ file_path = Path(__file__).parent / 'temperatur_data.csv'
 
 #read month 2 temperatures from data.csv
 temp_list = file_reader.read_from_file(file_path, 2)
-avg_temp = sum(temp_list)/len(temp_list)
-print(avg_temp)
+#avg_temp = sum(temp_list)/len(temp_list)
+#print(avg_temp)
+#print(temp_list)
 
 avg_temp = weather_functions.calculate_avg_temp(temp_list)
-print(avg_temp)
+#print(avg_temp)
 
 temp_list = file_reader.read_from_file(file_path, 0)
 spring_idx = weather_functions.when_is_spring(temp_list)
-print(spring_idx)
+#print(spring_idx)
 
 user_input = input('Vad vill du göra? Tryck 1 för medeltemperatur och 2 för vårens ankomst' + '\n')
 if int(user_input) == 1:
-    user_input = input('Vilken månad vill du beräkna medeltemperaturen för? Ange månadsnummer' + '\n')
+    user_input = int(input('Vilken månad vill du beräkna medeltemperaturen för? Ange månadsnummer' + '\n'))
     temp_list = file_reader.read_from_file(file_path, user_input)
     avg_temp = weather_functions.calculate_avg_temp(temp_list)
     print('Medeltemperaturen var ' + str(avg_temp) + ' grader i månad ' + str(user_input))
+elif int(user_input) == 2 :
+    print('Letar efter våren...' + '\n')
+    temp_list = file_reader.read_from_file(file_path, 0)
+    spring_idx = weather_functions.when_is_spring(temp_list)
+    print('Våren infaller på index ' + str(spring_idx))
