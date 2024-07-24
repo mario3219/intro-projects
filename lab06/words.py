3966a707d18152d4f7c192cb4778af4a7a169a94
Jonathan Olsson <jonathan.olsson8@gmail.com>
2024-07-24 02:59:08 +0200

    färdig med labb 6

--- lab06/words.py
+++ lab06/words.py
@@ -1,32 +1,46 @@
 import os
+import re
+import string
 
 def read_words(filename):
-    list = [line.split(" ") for line in filename]
+    list = [line.split() for line in filename]
     words = []
     for line in list:
-        for string in line:
-            string = string.strip("\n") and string.strip()
-            words.append(string)
+        for word in line:
+                word = word.lower()
+                word = word.strip(string.punctuation + string.whitespace)
+                words.append(word)
     return words
 
 def count_only(words, count_words):
-    counts = dict(count_words)
-    print(counts)
+    
+    counts = [0]*len(count_words)
+    my_zip = zip(count_words,counts)
+    counts = dict(my_zip)
+
     for word in words:
         if word in count_words:
-            counts[word] += 1
+            counts[word] = counts[word] + 1
     return counts
 
 def count_all_except(words, stopwords):
-    pass
+    counts = [0]*len(words)
+    my_zip = zip(words, counts)
+    counts = dict(my_zip)
+
+    for word in words:
+        if word not in stopwords:
+            counts[word] = counts[word] + 1
+    return counts
 
 def filter_hist(hist, min_count):
-    # ännu ej implementerad
-    pass
+    hist = [x for x in hist if x[1] > min_count]
+    return hist
 
 def sorted_hist(hist):
-    # ännu ej implementerad
-    pass
+    list = [(k, v) for k, v in hist.items()]
+    list.sort(key=lambda a: a[1])
+    return list
 
 # -----------------------------------------------------------
 
@@ -63,3 +77,32 @@ provinces = read_words(file)
 
 hist = count_only(words, provinces)
 print(hist['skåne'])
+
+# -----------------------------------------------------------
+
+# uppg 4
+
+filename = 'undantagsord.txt'
+filepath = os.path.join(os.path.dirname(__file__), filename)
+file = open(filepath, encoding='utf-8')
+stopwords = read_words(file)
+
+hist = count_all_except(words, stopwords)
+print(hist['han'])
+
+# -----------------------------------------------------------
+
+# uppg 5
+
+hist = sorted_hist(hist)
+index = len(hist)-1
+while index >= len(hist)-10:
+    print(hist[index])
+    index = index - 1
+
+# -----------------------------------------------------------
+
+# uppg 6
+
+hist = filter_hist(hist, 100)
+print(hist)
